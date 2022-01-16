a3969e0803edf3922a5f83a2108967e8d93af25d
Chris Gill <chris@chrisgill.net>
2022-01-16 09:18:34 -0500

    Actually pass the determined device to the model creation

--- hubconf.py
+++ hubconf.py
@@ -12,10 +12,8 @@ def delivery_vehicles(verbose=True,device=None):
 	This is a YOLOv5 based model to detect various types of delivery vehicles - UPS, US Postal Service, etc
 	"""
 
-	from pathlib import Path
 	from models.yolo import Model
-	from utils.downloads import attempt_download
-	from utils.general import check_requirements, intersect_dicts, set_logging
+	from utils.general import intersect_dicts, set_logging
 	from utils.torch_utils import select_device
 
 	# 3 channel, RGB images
@@ -28,7 +26,7 @@ def delivery_vehicles(verbose=True,device=None):
 		# Do we use a CUDA device?
 		device = select_device(('0' if torch.cuda.is_available() else 'cpu') if device is None else device)
 		# Set up the base yolov5 model
-		model = torch.hub.load('ultralytics/yolov5','yolov5s',classes=classes)
+		model = torch.hub.load('ultralytics/yolov5','yolov5s',classes=classes,device=device)
 		# Fetch the custom weights
 		checkpoint_url = "https://www.jumpbeacon.net/delivery_vehicles/delivery_vehicles_0.3.pt"
 		ckpt = torch.hub.load_state_dict_from_url(checkpoint_url,map_location=device)
